0da01e24022f504b745bb5c041469f091f6860b7
Andres Felipe Sanchez Lemus <andfsanchezlem@unal.edu.co>
2017-03-29 19:37:03 -0500

    arreglo de nombres en declaracion de relaciones

--- app/models/materium.rb
+++ app/models/materium.rb
@@ -4,7 +4,7 @@ class Materium < ApplicationRecord
 	has_many :enfoque_materium
 	has_many :enfoque,through: :enfoque_materium
 	has_many :materia_investigacion
-	has_many :grupo_de_investigacion,through :materia_investigacion
+	has_many :grupo_investigacion,through :materia_investigacion
 
 	validates :nombre,presence: true
     validates :nombre, uniqueness: true
