d9ad53339b964cac05bad226e36719aae86d914a
sebastian medina <srmedinac@unal.edu.co>
2017-03-30 18:19:47 -0500

    queries, falta anadir campos en db

--- app/models/materium.rb
+++ app/models/materium.rb
@@ -10,20 +10,29 @@ class Materium < ApplicationRecord
   validates :nombre, uniqueness: true
 
 	def porcentaje_disciplinares
-		numDisc = Materium.where("tipo = diciplinar").group(:tipo).count.length
-		numApproved = MateriasTomadas.where("tipo = diciplinar").group(:tipo).count.length
+		numDisc = materia.group_by(:tipo).having("tipo = disciplinar").count.length
+		numApproved = materia.joins(:historia_academicas).where("materia.tipo = disciplinar").count.length   #materia.tipo ? tipo
+		#numApproved = HistoriaAcademicas.where("tipo = diciplinar").group(:tipo).count.length
 		return numApproved/numDisc*100
 	end
 	def porcentaje_fundamentacion
-		numFun = Materium.where("tipo = fundamentacion").group(:tipo).count.length
-		numApproved = MateriasTomadas.where("tipo = fundamentacion").group(:tipo).count.length
+		numFun = materia.group_by(:tipo).having("tipo = fundamentacion").count.length
+		numApproved = materia.joins(:historia_academicas).where("materia.tipo = fundamentacion").count.length
 		return numApproved/numFun*100
 	end
 	def porcentaje_libre_eleccion
-		numLib = Materium.where("tipo = libre").group(:tipo).count.length
-		numApproved = MateriasTomadas.where("tipo = libre").group(:tipo).count.length
+		numLib = materia.where("tipo = libre").group(:tipo).count.length
+		numApproved = materia.joins(:historia_academicas).where("materia.tipo = libre").count.length
 		return numApproved/numLib*100
 	end
-
-
+	def get_prof_grado(grado)
+			prof_id = profesor_grados.where(:trabajo_grado_id = grado)
+			prof_name = profesors.where(:profesor_id = prof_id).select("nombre")
+			return prof_name
+	end
+	def get_prof_investigacion(investigacion_id)
+			prof_id = profesor_investigacions.where(:grupo_investigacion_id = investigacion_id)
+			prof_name = profesors.where(:profesor_id = prof_id).select("nombre")
+			return prof_name
+	end
 end
