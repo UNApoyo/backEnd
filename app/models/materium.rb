1efb7191d57a9974dc6e06f4587e21fa44ec67ff
sebastian medina <srmedinac@unal.edu.co>
2017-03-30 19:41:26 -0500

    fixes y mas queries agregados

--- app/models/materium.rb
+++ app/models/materium.rb
@@ -10,42 +10,46 @@ class Materium < ApplicationRecord
   validates :nombre, uniqueness: true
 
 	def porcentaje_disciplinares
-		numDisc = materia.group_by(:tipo).having("tipo = disciplinar").count.length
-		numApproved = materia.joins(:historia_academicas).where("materia.tipo = disciplinar").count.length   #materia.tipo ? tipo
+		numDisc = Materium.group_by(:tipo).having("tipo = disciplinar").count.length
+		numApproved = Materium.joins(:historia_academica).where("materia.tipo = disciplinar").count.length   #materia.tipo ? tipo
 		#numApproved = HistoriaAcademicas.where("tipo = diciplinar").group(:tipo).count.length
 		return numApproved/numDisc*100
 	end
 	def porcentaje_fundamentacion
-		numFun = materia.group_by(:tipo).having("tipo = fundamentacion").count.length
-		numApproved = materia.joins(:historia_academicas).where("materia.tipo = fundamentacion").count.length
-		return numApproved/numFun*100
+			numFun = Materium.group_by(:tipo).having("tipo = fundamentacion").count.length
+			numApproved = Materium.joins(:historia_academica).where("materia.tipo = fundamentacion").count.length
+			return numApproved/numFun*100
 	end
 	def porcentaje_libre_eleccion
-		numLib = materia.where("tipo = libre").group(:tipo).count.length
-		numApproved = materia.joins(:historia_academicas).where("materia.tipo = libre").count.length
-		return numApproved/numLib*100
+			numLib = Materium.where("tipo = libre").group(:tipo).count.length
+			numApproved = Materium.joins(:historia_academica).where("materia.tipo = libre").count.length
+			return numApproved/numLib*100
 	end
 	def get_prof_grado(grado)
-			prof_id = profesor_grados.where(:trabajo_grado_id = grado)
-			prof_name = profesors.where(:profesor_id = prof_id).select("nombre")
+			prof_id = profesor_grado.where(:trabajo_grado_id = grado)
+			prof_name = profesor.where(:profesor_id = prof_id).select("nombre")
 			return prof_name
 	end
 	def get_prof_investigacion(investigacion_id)
-			prof_id = profesor_investigacions.where(:grupo_investigacion_id = investigacion_id)
-			prof_name = profesors.where(:profesor_id = prof_id).select("nombre")
+			prof_id = profesor_investigacion.where(:grupo_investigacion_id = investigacion_id)
+			prof_name = profesor.where(:profesor_id = prof_id).select("nombre")
 			return prof_name
 	end
 	def get_promedio_disc
-			calificaciones = historia_academicas.joins(:materia).where("materia.tipo = disciplinar").average("historia_academicas.calificacion")
+			calificaciones = historia_academica.joins(:materia).where("materia.tipo = disciplinar").average("historia_academicas.calificacion")
 			return calificaciones
 	end
 	def get_promedio_fun
-			calificaciones = historia_academicas.joins(:materia).where("materia.tipo = fundamentacion").average("historia_academicas.calificacion")
+			calificaciones = historia_academica.joins(:materia).where("materia.tipo = fundamentacion").average("historia_academicas.calificacion")
 			return calificaciones
 	end
 	def get_promedio_lib
-			calificaciones = historia_academicas.joins(:materia).where("materia.tipo = libre").average("historia_academicas.calificacion")
+			calificaciones = historia_academica.joins(:materia).where("materia.tipo = libre").average("historia_academicas.calificacion")
 			return calificaciones
 	end
-
+	def get_grupo_investigacion_carrera(id_carrera)
+			grupos = carrera_investigacion.where(:carrera_id = id_carrera).select("grupo_investigacion_id")
+			name_grupos = grupo_investigacion.where(nombre: grupos)
+			return name_grupos
+	end
 end
